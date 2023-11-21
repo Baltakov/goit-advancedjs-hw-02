4cefb6f72b199002cc0819913b5e679c93a5acd6
Baltakov <tira2500@gmail.com>
2023-11-21 12:14:48 +0200

    Promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  let delay = Number(formData.get('delay'));
+  const step = Number(formData.get('step'));
+  const amount = Number(formData.get('amount'));
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          title: 'Success',
+          message: `Fulfilled promise ${position} in ${delay}ms`,
+        });
+        console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: 'Error',
+          message: `Rejected promise ${position} in ${delay}ms`,
+        });
+        console.log(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+});
